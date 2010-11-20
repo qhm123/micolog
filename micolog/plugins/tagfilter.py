3abc996959796c71a17b0ccd356851fdef95cdd0
qhm123 <qhm123@8fbaecf5-001a-4f14-1e68-2a97a11ed4d1>
2010-11-20 05:49:40 +0000

    制作了tagfilter插件，根据tag个数过滤tag的插件。修改了主题meta。
    git-svn-id: https://micolog-qhm.googlecode.com/svn/trunk@10 8fbaecf5-001a-4f14-1e68-2a97a11ed4d1

---
+++ micolog/plugins/tagfilter.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+import urllib
+
+from micolog_plugin import *
+from model import Tag
+
+def my_urlencode(s):
+    p = repr(s).replace(r'\x', '%')
+    return p[1:-1]
+
+class tagfilter(Plugin):
+    def __init__(self):
+        Plugin.__init__(self, __file__)
+        self.author="鸣"
+        self.authoruri="http://qhm123.appspot.com"
+        self.uri=""
+        self.description="filter tag by count"
+        self.register_filter('tagfilter', self.tagfilter)
+        self.name="tagfilter"
+        self.version="0.1"
+        
+    def get(self, page):
+        filter_count = OptionSet.getValue("tagfilter_filter_count", default='2')
+        return u'''<h3>设置tagfilter</h3>
+               <form action="" method="post">
+               <div style="float:left;width:130px;">过滤个数:</div> <input name="filter_count" value="%s" /><br />
+               <br />
+               <input type="submit" value="确定">
+               </form>
+               <br />
+               Powered By <a href="http://qhm123.appspot.com" target="_blank">鸣</a>
+               ''' % (filter_count,)
+    
+    def post(self, page):
+        filter_count = page.param("filter_count")
+        OptionSet.setValue("tagfilter_filter_count", filter_count)
+
+        return self.get(page)
+        
+    def tagfilter(self, content, blog=None, *arg1, **arg2):
+        filter_count = OptionSet.getValue("tagfilter_filter_count", default='2')
+        filter_tags = Tag.all().filter('tagcount >', int(filter_count)).fetch(limit=1000)
+        html = "<a title='%d pages' href='/tag/%s' style='font-size:%dpx;'>%s</a>"
+        tag_htmls = [html % (tag.tagcount, urllib.quote(tag.tag.encode('utf8')), tag.tagcount+10, tag.tag) for tag in filter_tags]
+        data = ' '.join(tag_htmls)
+        return data
