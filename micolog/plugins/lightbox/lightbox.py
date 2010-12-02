bc382a27d748e1f1337d7e0f66adc9fdbf5644ce
qhm123 <qhm123@8fbaecf5-001a-4f14-1e68-2a97a11ed4d1>
2010-12-02 13:05:46 +0000

    添加了自动添加效果代码功能。修改了readme.txt。
    git-svn-id: https://micolog-qhm.googlecode.com/svn/trunk@17 8fbaecf5-001a-4f14-1e68-2a97a11ed4d1

--- micolog/plugins/lightbox/lightbox.py
+++ micolog/plugins/lightbox/lightbox.py
@@ -6,24 +6,25 @@ from model import *
 class lightbox(Plugin):
     def __init__(self):
         Plugin.__init__(self, __file__)
-        self.author="鸣"
-        self.authoruri="http://qhm123.appspot.com"
-        self.uri=""
-        self.description="overlay images on the pages"
+        self.author = "鸣"
+        self.authoruri = "http://qhm123.appspot.com"
+        self.uri = ""
+        self.description = "overlay images on the pages"
         self.register_filter('footer', self.footer)
         self.register_urlzip('/lightbox/(.*)', 'lightbox.zip')
-        self.name="lightbox"
-        self.version="0.1"
+        self.name = "lightbox"
+        self.version = "0.2"
 
-    def get(self,page):
-        #markup = OptionSet.getValue("markup", default="")
-        loadjquery = OptionSet.getValue("loadjquery", default='true') #是否加载jquery
-        bgcolor = OptionSet.getValue("bgcolor", default='#000') #背景颜色
-        bgopacity = OptionSet.getValue("bgopacity", default='0.8') #背景透明度
-        fixednavi = OptionSet.getValue("fixednavi", default='false') #导航按钮是否固定
-        speed = OptionSet.getValue("speed", default='400') #展开速度
+    def get(self, page):
+        entryselector = OptionSet.getValue("lightbox_entryselector", default='false')           # 正文处选择器
+        loadjquery = OptionSet.getValue("lightbox_loadjquery", default='true')                  # 是否加载jquery
+        bgcolor = OptionSet.getValue("lightbox_bgcolor", default='#000')                        # 背景颜色
+        bgopacity = OptionSet.getValue("lightbox_bgopacity", default='0.8')                     # 背景透明度
+        fixednavi = OptionSet.getValue("lightbox_fixednavi", default='false')                   # 导航按钮是否固定
+        speed = OptionSet.getValue("speed", default='400')                                      # 展开速度
         return u'''<h3>设置lightbox</h3>
                <form action="" method="post">
+               <div style="float:left;width:130px;">正文选择器:</div><input name="entryselector" value="%s" />(格式：.(#)正文处div的class(id)名称-启用；false-禁用，默认值false)<br />
                <div style="float:left;width:130px;">是否加载jquery:</div> <input name="loadjquery" value="%s" />(格式：false-不加载 ;true-加载，默认值true)<br />
                <div style="float:left;width:130px;">背景颜色:</div><input name="bgcolor" value="%s" />(格式：#rrggbb，默认值#000)<br />
                <div style="float:left;width:130px;">背景透明度:</div> <input name="bgopacity" value="%s" />(格式：0.X，其中X只能是从0到9的整数，默认值0.8)<br />
@@ -34,30 +35,31 @@ class lightbox(Plugin):
                </form>
                <br />
                Powered By <a href="http://qhm123.appspot.com" target="_blank">鸣</a>
-               ''' % (loadjquery, bgcolor, bgopacity, fixednavi, speed)
+               ''' % (entryselector, loadjquery, bgcolor, bgopacity, fixednavi, speed)
 
-    def post(self,page):
-        #markup = page.param("markup")
-        loadjquery = page.param("loadjquery")
-        bgcolor = page.param("bgcolor") #背景颜色
-        bgopacity = page.param("bgopacity") #背景透明度
-        fixednavi = page.param("fixednavi") #导航按钮是否固定
-        speed = page.param("speed") #展开速度
-        #OptionSet.setValue("markup", markup)
-        OptionSet.setValue("loadjquery", loadjquery)
-        OptionSet.setValue("bgcolor", bgcolor) #背景颜色
-        OptionSet.setValue("bgopacity", bgopacity) #背景透明度
-        OptionSet.setValue("fixednavi", fixednavi) #导航按钮是否固定
-        OptionSet.setValue("speed", speed) #展开速度
+    def post(self, page):
+        entryselector = page.param("entryselector")     # 正文处选择器
+        loadjquery = page.param("loadjquery")           # 是否加载jquery
+        bgcolor = page.param("bgcolor")                 # 背景颜色
+        bgopacity = page.param("bgopacity")             # 背景透明度
+        fixednavi = page.param("fixednavi")             # 导航按钮是否固定
+        speed = page.param("speed")                     # 展开速度
+        
+        OptionSet.setValue("lightbox_entryselector", entryselector) # 正文处选择器
+        OptionSet.setValue("lightbox_loadjquery", loadjquery)       # 是否加载jquery
+        OptionSet.setValue("lightbox_bgcolor", bgcolor)             # 背景颜色
+        OptionSet.setValue("lightbox_bgopacity", bgopacity)         # 背景透明度
+        OptionSet.setValue("lightbox_fixednavi", fixednavi)         # 导航按钮是否固定
+        OptionSet.setValue("lightbox_speed", speed)                 # 展开速度
         return self.get(page)
 
     def footer(self, content, blog=None, *arg1, **arg2):
-        #markup = OptionSet.getValue("markup", default="")
-        loadjquery = OptionSet.getValue("loadjquery", default='true').encode("utf8") #是否加载jquery
-        bgcolor = OptionSet.getValue("bgcolor", default='#000').encode("utf8") #背景颜色
-        bgopacity = OptionSet.getValue("bgopacity", default='0.8').encode("utf8") #背景透明度
-        fixednavi = OptionSet.getValue("fixednavi", default='false').encode("utf8") #导航按钮是否固定
-        speed = OptionSet.getValue("speed", default='400').encode("utf8") #展开速度
+        entryselector = "'"+OptionSet.getValue("lightbox_entryselector", default='false').encode("utf8")+"'"# 正文处选择器 
+        loadjquery = OptionSet.getValue("lightbox_loadjquery", default='true').encode("utf8")       # 是否加载jquery
+        bgcolor = OptionSet.getValue("lightbox_bgcolor", default='#000').encode("utf8")             # 背景颜色
+        bgopacity = OptionSet.getValue("lightbox_bgopacity", default='0.8').encode("utf8")          # 背景透明度
+        fixednavi = OptionSet.getValue("lightbox_fixednavi", default='false').encode("utf8")        # 导航按钮是否固定
+        speed = OptionSet.getValue("lightbox_speed", default='400').encode("utf8")                  # 展开速度
         host = blog.baseurl.encode("utf8")
         if loadjquery == 'true':
             jquery = '<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>'
@@ -65,22 +67,41 @@ class lightbox(Plugin):
             jquery = ''
         jquery = jquery.encode("utf8")
         data = content + '''<link rel="stylesheet" type="text/css" href="/lightbox/css/jquery.lightbox-0.5.css" media="screen" />
-%s
+%(jquery)s
 <script type="text/javascript" src="/lightbox/js/jquery.lightbox-0.5.pack.js"></script>
 <script type="text/javascript">
 $(function() {
-        $('a[rel=lightbox]').lightBox({
-            overlayBgColor: '%s',
-            overlayOpacity: %s,
-            fixedNavigation: %s,
-            containerResizeSpeed: %s,
-            imageLoading: '%s/lightbox/images/lightbox-ico-loading.gif',
-            imageBtnClose: '%s/lightbox/images/lightbox-btn-close.gif',
-            imageBtnPrev: '%s/lightbox/images/lightbox-btn-prev.gif',
-            imageBtnNext: '%s/lightbox/images/lightbox-btn-next.gif',
-            imageBlank: '%s/lightbox/images/lightbox-blank.gif'
-        }); // Select all links that contains lightbox in the attribute rel
+    allanchors = null;
+    if(%(entryselector)s == 'false'){
+        allanchors = $('a[rel=lightbox]');
+    } else{
+        $(%(entryselector)s+' img').each(function(){
+            img = $(this);
+            if(img.parent('a').length < 1){
+                src = img.attr('src');
+                img.wrap('<a href='+src+'></a>');
+            }
+        });
+        allanchors = $(%(entryselector)s+' a:has(img)');
+    }
+    allanchors.lightBox({
+        overlayBgColor: '%(bgcolor)s',
+        overlayOpacity: %(bgopacity)s,
+        fixedNavigation: %(fixednavi)s,
+        containerResizeSpeed: %(speed)s,
+        imageLoading: '%(host)s/lightbox/images/lightbox-ico-loading.gif',
+        imageBtnClose: '%(host)s/lightbox/images/lightbox-btn-close.gif',
+        imageBtnPrev: '%(host)s/lightbox/images/lightbox-btn-prev.gif',
+        imageBtnNext: '%(host)s/lightbox/images/lightbox-btn-next.gif',
+        imageBlank: '%(host)s/lightbox/images/lightbox-blank.gif'
+    });
 });
 </script>
-'''.encode("utf8") % (jquery, bgcolor, bgopacity, fixednavi, speed, host, host, host, host, host)
+'''.encode("utf8") % {'jquery': jquery,
+                      'entryselector': entryselector,
+                      'bgcolor': bgcolor,
+                      'bgopacity': bgopacity,
+                      'fixednavi': fixednavi,
+                      'speed': speed,
+                      'host': host}
         return data
--- micolog/plugins/lightbox/readme.txt
+++ micolog/plugins/lightbox/readme.txt
@@ -1,27 +1,43 @@
-lightbox��һ��micolog�Ĳ��������ʵ��lightBoxͼƬ�鿴Ч�����ò����������ֻ��6kb��JavaScript���룬���ݴ󲿷��������
+lightbox是一个micolog的插件，用于实现lightBox图片查看效果。该插件采用最精简的只有6kb的JavaScript代码，兼容大部分浏览器。
 
-ʹ��˵����
-1.�����صĲ����ѹ����
-2.�����Ŀ¼����micolog��pluginsĿ¼�¡�
-3.�ϴ����micolog��
-4.�ں�̨����lightbox�����
-����lightbox�������������ˡ�
+使用说明：
+1.将下载的插件解压缩。
+2.将插件目录放入micolog的plugins目录下。
+3.上传你的micolog。
+4.在后台激活lightbox插件。
+这样lightbox插件就设置完成了。
 
-ע�⣺��ȷ����������е�base.html�ļ��д���{%mf footer%}{%endmf%}��ǩ��
-���û������base.html��footer�����ӣ�����֧���б������ҵ�������¡�
+注意：请确保你的主题中的base.html文件中存在{%mf footer%}{%endmf%}标签，
+如果没有请在base.html中footer下添加，主题支持列表参阅我的相关文章。
 
-��ϸ���ã�
-1.���lightbox������lightbox����ҳ�档
-2.����5��ѡ��������á�
-3.���ڵ�һ�����Ƿ����jQuery��ѡ���Ҫȷ�����base.html�ļ���head�����Ƿ��Ѿ�������jQuery������Ѽ��أ���������Ϊfalse�������ظ����ء�
-4.ʣ���ĸ�ѡ��ǵ���ͼƬ������ʾЧ��ʹ�á�
+详细设置：
+1.点击lightbox，进入lightbox设置页面。
+2.共有5个选项可以设置。
+3.对于第一个“是否加载jQuery”选项，需要确认你的base.html文件的head部分是否已经加载了jQuery，如果已加载，可以设置为false，放置重复加载。
+4.剩余四个选项，是调节图片弹出显示效果使用。
 
-ʹ��ʾ����<a href="http://micolog.appspot.com/micolog/micolog.jpg" rel="lightbox" /><img src="http://micolog.appspot.com/micolog/micolog.jpg" /> </a>
-˵�������������<img />��ǩ�ⲿ����һ������ı�ǩ<a>��<a>��href����Ϊչ�����ͼƬ��ַ��<img>��src����ΪĬ����ʾ��ͼƬ��ַ��
+使用示例：<a href="http://micolog.appspot.com/micolog/micolog.jpg" rel="lightbox" /><img src="http://micolog.appspot.com/micolog/micolog.jpg" /> </a>
+说明：就是在你的<img />标签外部添加一个外包的标签<a>，<a>的href属性为展开后的图片地址。<img>的src属性为默认显示的图片地址。
 
-����Ч���ɲο��ҵĲ��͵����ҳ��http://qhm123.appspot.com/2010/10/9/micolog-lightbox-picture-plugin.html
+具体效果可参看我的博客的这个页面http://qhm123.appspot.com/2010/10/9/micolog-lightbox-picture-plugin.html
 
-���κ��������bug���Ե�http://qhm123.appspot.com/2010/10/9/micolog-lightbox-picture-plugin.html���ԣ�
-�һᾡ��ظ����޸���лл����֧�֡�
-
+有任何问题或发现bug可以到http://qhm123.appspot.com/2010/10/9/micolog-lightbox-picture-plugin.html留言，
+我会尽快回复并修复，谢谢您的支持。
 2010-10-14
+
+0.2更新：
+添加了自动添加特效代码功能，默认关闭。开启需设置。
+设置方法：在插件参数修改处修改“正文选择器”中内容：
+	1.打开你的主题模板中的single.html文件。
+	2.找到{{ entry.content }}的上一级div标签。
+	3.在“正文选择器”中输入"."加上这个div标签的class中的内容。
+		示例：对于default主题是class名字为en_body，则输入内容为.en_body。
+	4.如果没有class属性，可以找到div的id属性，在”正文选择器“中输入"#"和id名称。
+		示例：假设id="post"，则填入”#post“
+	5.点击确定按钮，设置完成。
+	
+有任何问题或发现bug可以到http://qhm123.appspot.com/2010/10/9/micolog-lightbox-picture-plugin.html留言，
+我会尽快回复并修复，谢谢您的支持。
+2010-12-02
+	
+
