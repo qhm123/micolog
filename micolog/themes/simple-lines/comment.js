b42bf20a71eba3e026a5bdaff119666f811b786b
qhm123 <qhm123@8fbaecf5-001a-4f14-1e68-2a97a11ed4d1>
2010-12-03 12:33:03 +0000

    添加了留言验证码。
    git-svn-id: https://micolog-qhm.googlecode.com/svn/trunk@19 8fbaecf5-001a-4f14-1e68-2a97a11ed4d1

--- micolog/themes/simple-lines/comment.js
+++ micolog/themes/simple-lines/comment.js
@@ -12,6 +12,7 @@
         $('#email').val(data[1]);
         $('#url').val(data[2]);
     };
+    $('#check').load('/checkcode/');
     
     $('#commentform').ajaxForm({
         type: 'post',
@@ -30,6 +31,15 @@
                     return false;
                 }
             }
+            
+			if($('#checkarea').css('display') == 'block'){
+				if(!form.checkret.value){
+					alert('请输入验证码！');
+					form.checkret.focus();
+					return false;
+				}
+			}
+
             if (!form.comment.value) {
                 showinfo('请输入留言内容');
                 form.comment.focus();
@@ -37,6 +47,7 @@
             }
             
             $("#submit").attr('disabled', true);
+            $("#submit").val('正在提交留言……');
             
             return true;
         },
@@ -45,12 +56,25 @@
             if (data[0]) {
                 /*document.cookie=data[2];*/
                 alert('留言提交成功！');
+                $("#submit").val('提交');
                 add_comment(data[1]);
                 
                 //$('#s_msg').text('留言提交成功！');
                 $('#comment').val('');
+                if($('#checkarea').css('display') == 'block'){
+                    if($("#check_type").val() > 0){
+                    	get_check_area($("#check_type").val());
+                    }
+                }
+                $('#checkret').val('');
                 location = "#comments";
-            }
+            }else{
+				if(data[1] == -102){
+					showinfo('验证码错误。');
+				}
+				$('#s_msg').text('留言提交失败！');
+				$('#checkret').focus();
+			}
         }
     });
 })
@@ -60,12 +84,10 @@ function get_check_area(type){
         $('#check').load('/checkcode/');
         $('#checkarea').show();
     }
-    else 
-        if (type == 2) {
-            $('#check').html('<img id="checkimg" src="/checkimg/" style="border:0px;padding:0;float:left;margin-right:8px" title="点击图片切换" onclick="reloadCheckImage();"/>');
-            $('#checkarea').show();
-        }
-    
+    else if (type == 2) {
+        $('#check').html('<img id="checkimg" src="/checkimg/" style="border:0px;padding:0;float:left;margin-right:8px" title="点击图片切换" onclick="reloadCheckImage();"/>');
+        $('#checkarea').show();
+    }
 }
 
 function reloadCheckImage(){
